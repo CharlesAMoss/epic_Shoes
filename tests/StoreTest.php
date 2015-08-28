197abfc76ccf8272595d02e782a4b1716db32326
CharlesAMoss <charlesamoss@yahoo.com>
2015-08-28 11:02:27 -0700

    add store get id and fail

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -25,14 +25,16 @@
         {
             //Arrange
             $store_name = "Gary's Shoes and Accessories for Today's Woman";
-            $id = null;
+            $id = 1;
             $test_store = new Store($store_name, $id);
 
             //Act
             $result = $test_store->getName();
+            $result2 = $test_store->getId();
 
             //Assert
             $this->assertEquals($store_name, $result);
+            $this->assertEquals($id, $result);
         }//end test
 
     }//end of class
