a447ffa707c3896978a2f8286c62cd7a53c3a540
CharlesAMoss <charlesamoss@yahoo.com>
2015-08-28 11:31:07 -0700

    add store save test and fail

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -51,7 +51,21 @@
 
             //Assert
             $this->assertEquals($new_name, $result);
+        }//end test
 
+        function test_store_save()
+        {
+            //Arrange
+            $store_name = "Gary's Shoes and Accessories for Today's Woman";
+            $id = 1;
+            $test_store = new Store($store_name, $id);
+
+            //Act
+            $test_store->save();
+
+            //Assert
+            $result = Store::getAll();
+            $this->assertEquals($test_store, $result[0]);
         }//end test
 
     }//end of class
