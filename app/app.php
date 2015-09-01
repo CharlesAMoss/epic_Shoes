4579247f75498e647202dde5510da69b276c7b6d
CharlesAMoss <charlesamoss@yahoo.com>
2015-08-31 22:40:43 -0700

    add routes and twigs for brand full commit

--- app/app.php
+++ app/app.php
@@ -20,15 +20,43 @@
         return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
+    $app->get("/all_brands", function() use ($app) {
+        return $app['twig']->render('all_brands.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+    $app->get("/brand/{id}", function($id) use ($app) {
+       $store = Brand::find($id);
+       return $app['twig']->render('brand.html.twig, ', array('stores' => Store::getAll(), 'brands' => $store->getBrands(), 'all_brands' => Brands::getAll()));
+   });
+
+    $app->get("/store_brand", function() use ($app) {
+        return $app['twig']->render('store_brand.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
     $app->post("/store_brand", function() use ($app) {
-        $store_name = $_POST['stone_name'];
+        $store_name = $_POST['store_name'];
         $store = new Store($store_name, $id = null);
         $store->save();
+
         $brand_name = $_POST['brand_name'];
         $brand = new Brand($brand_name, $id = null);
         $brand->save();
+
         $result = $store->addBrand($brand);
-        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+
+        return $app['twig']->render('store_brand.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+    $app->get("/brand_store", function() use ($app) {
+        return $app['twig']->render('brand_store.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+    $app->post("/brand_store", function() use ($app) {
+        $brand = Brand::find($_POST['brand_id']);
+        $store = Store::find($_POST['store_id']);
+        $brand->addStore($store);
+
+        return $app['twig']->render('brand_store.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
     $app->post("/delete_all", function() use ($app) {
